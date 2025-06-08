415c037e2a10c9a2144b4523ed532eef85506edd
Yi-Hsien Lin <dora4300605497079@gmail.com>
2025-06-08 22:29:13 +0800

    20250608

---
+++ long-exposure/src/camera_simulation.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def srgb_to_linear(img_srgb):
+    """Convert an sRGB image to linear RGB"""
+    img_srgb = img_srgb / 255.0
+    threshold = 0.04045
+    below = img_srgb <= threshold
+    above = img_srgb > threshold
+
+    img_linear = np.zeros_like(img_srgb)
+    img_linear[below] = img_srgb[below] / 12.92
+    img_linear[above] = ((img_srgb[above] + 0.055) / 1.055) ** 2.4
+
+    return img_linear
+
+def linear_to_srgb(img_linear):
+    """Convert a linear RGB image to sRGB"""
+    threshold = 0.0031308
+    below = img_linear <= threshold
+    above = img_linear > threshold
+
+    img_srgb = np.zeros_like(img_linear)
+    img_srgb[below] = img_linear[below] * 12.92
+    img_srgb[above] = 1.055 * (img_linear[above] ** (1 / 2.4)) - 0.055
+
+    return np.clip(img_srgb * 255.0, 0, 255).astype(np.uint8)
+
+def simulate_camera_response(image_list: list[np.ndarray]) -> np.ndarray:
+    """
+    Simulate camera response for long exposure:
+    - Convert to linear domain
+    - Accumulate and average
+    - Convert back to sRGB
+
+    Args:
+        image_list: List of RGB images in uint8, shape (H, W, 3)
+
+    Returns:
+        Averaged image (sRGB), shape (H, W, 3), dtype uint8
+    """
+    linear_sum = None
+    for img in image_list:
+        linear = srgb_to_linear(img.astype(np.float32))
+        if linear_sum is None:
+            linear_sum = linear
+        else:
+            linear_sum += linear
+    avg_linear = linear_sum / len(image_list)
+    return linear_to_srgb(avg_linear)
